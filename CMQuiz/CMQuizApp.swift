d4f45b833bdc54d3209d63ccd84ae456387019a7
MaskerDog <37284851+MaskerDog@users.noreply.github>
2022-04-14 17:50:27 +0900

    Initial Commit

---
+++ CMQuiz/CMQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  CMQuizApp.swift
+//  CMQuiz
+//
+//  Created by npc on 2022/04/14.
+//
+
+import SwiftUI
+
+@main
+struct CMQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CMQuiz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CMQuiz
+//
+//  Created by npc on 2022/04/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
